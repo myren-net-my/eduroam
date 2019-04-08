b0a9e6691189703cf9925eeab8a2e6389df94864
hamid-myren <49390480+hamid-myren@users.noreply.github.com>
2019-04-08 22:53:11 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,5 +3,5 @@ export CLIENT_IP_ADDRESS="172.31.52.2"
 export CLIENT_SECRET="NbsROz14fz3wFjDe"
 
 ## proxy.conf
-export LOCAL_SCHOOL_REALM="instition.edu.my"
+export LOCAL_SCHOOL_REALM="institution.edu.my"
 export PARENT_IP_ADDRESSES=("203.80.16.20" "203.80.16.18")
