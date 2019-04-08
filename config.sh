a2b045ea300e9b29c2acf66ed0c923390b065bf3
hamid-myren <49390480+hamid-myren@users.noreply.github.com>
2019-04-08 22:51:05 +0800

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,7 @@
+## clients.conf
+export CLIENT_IP_ADDRESS="172.31.52.2"
+export CLIENT_SECRET="NbsROz14fz3wFjDe"
+
+## proxy.conf
+export LOCAL_SCHOOL_REALM="instition.edu.my"
+export PARENT_IP_ADDRESSES=("162.105.129.2" "162.105.129.5")
