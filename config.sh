17f63dc836afcd4872c1bf33bc2c5963efcff6bb
hamid-myren <49390480+hamid-myren@users.noreply.github.com>
2019-04-15 09:18:30 +0800

    Delete config.sh

--- config.sh
+++
@@ -1,7 +0,0 @@
-## clients.conf
-export CLIENT_IP_ADDRESS="172.31.52.2"
-export CLIENT_SECRET="NbsROz14fz3wFjDe"
-
-## proxy.conf
-export LOCAL_SCHOOL_REALM="institution.edu.my"
-export PARENT_IP_ADDRESSES=("203.80.16.20" "203.80.16.18")
